a49dddbb0ca7d6ebf5cc08fdf6fed4cfd6ecfa0a
Ayush Yadav <ayushyadav0729@gmail.com>
2026-08-19 22:14:46 +0530

    Add subject model and UUID relationships

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,13 +1,39 @@
 # this defines how we will write the data in signup database 
 
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from datetime import datetime
 from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String)
     email = Column(String, unique=True)
     password = Column(String)
+
+    subjects = relationship("Subject", back_populates="owner")
+
+class Subject(Base):
+    __tablename__ = "subjects"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    user_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    name = Column(String, nullable=False)
+
+    created_at = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
+
+    owner = relationship("User", back_populates="subjects")
+
